874fcc2f84b9f89b900546670f93899cc6b7b5a6
Preecha Tangsongsermsuk <preecha.t@linecorp.com>
2018-09-27 11:29:13 +0700

    Finish second chapter -Able to get element and child element -Able to click hidden item with force

--- cypress/integration/02-adding-items/spec.js
+++ cypress/integration/02-adding-items/spec.js
@@ -9,8 +9,7 @@ it('starts with zero items', () => {
   // TODO check if the list is empty initially
   // find the selector for the individual TODO items in the list
   // use cy.get(...) and it should have length of 0
-  cy.get('.todo')
-    .should('have.length', 0)
+  cy.get('.todo').should('have.length', 0)
 })
 
 it('adds two items', () => {
@@ -19,56 +18,48 @@ it('adds two items', () => {
   //    type text and "enter"
   //    assert that the new Todo item
   //    has been added added to the list
-  try {
-    cy.get('.todo')
-  } catch (e) {
-    //means nothing show here yet
-    if ($el.length == 0) {
-      cy.get('.new-todo')
-        .type('test1{enter}')
-      cy.get('.todo')
-        .should('have.length', 1)
-    } else if ($el.length == 1) {
-      cy.get('.new-todo')
-        .type('test2{enter}')
-      cy.get('.todo')
-        .should('have.length', 2)
-    }
-
-  }
-  // cy.get('.todo').then(($el) => {
-  //   if ($el.length == 0) {
-  //     cy.get('.new-todo')
-  //       .type('test1{enter}')
-  //     cy.get('.todo')
-  //       .should('have.length', 1)
-  //   } else if ($el.length == 1) {
-  //     cy.get('.new-todo')
-  //       .type('test2{enter}')
-  //     cy.get('.todo')
-  //       .should('have.length', 2)
-  //   }
-  // })
+  cy.get('.new-todo').type('first item{enter}')
+  cy.contains('li.todo', 'first item').should('be.visible')
+  cy.get('.new-todo').type('second item{enter}')
+  cy.contains('li.todo', 'second item').should('be.visible')
 })
 
 function addTodo(input) {
-  cy.get('.new-todo')
-    .type(input + '{enter}')
+  cy.get('.new-todo').type(input + '{enter}')
 }
 
 it('can add many items', () => {
   const N = 5
-  for (let k = 0; k < N; k += 1) {
-    // add an item
-    // probably want to have a reusable function to add an item!
-  }
-  // check number of items
+  var data = 0;
+  cy.get('li.todo').then(($el) => {
+    data = $el.length
+    console.log('data: ' + data)
+    for (let k = 0; k < N; k += 1) {
+      // add an item
+      // probably want to have a reusable function to add an item!
+      addTodo('item_' + k)
+    }
+    var expectItem = data + N
+    // check number of items
+    cy.get('li.todo').should('have.length', expectItem)
+  })
+
 })
 
 it('can mark items as completed', () => {
   // add a few items
+  addTodo('simple')
   // mark items as completed
+  cy
+    .contains('li.todo', 'simple')
+    .should('exist')
+    .find('input[type="checkbox"]')
+    .check()
   // select completed items and confirm their number
+  cy.contains('li.todo', 'simple').find('.destroy').click({
+    force: true
+  })
+  cy.contains('li.todo', 'simple').should('not.exist')
 })
 
 // what a challenge?
